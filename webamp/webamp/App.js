175870c5f0947d876d9c1e4a222848a928279ba0
OwnedByWuigi <120682622+OwnedByWuigi@users.noreply.github.com>
2024-07-26 22:46:49 +0100

    Add files via upload

---
+++ webamp/webamp/App.js
@@ -0,0 +1,21 @@
+import Html from "../../libs/html.js";
+
+const pkg = {
+  name: "Webamp",
+  type: "app",
+  privs: 0,
+  start: async function (Root) {
+    new Html("div").id("app").appendTo("body");
+
+    const app = document.getElementById("app")
+    const webamp = new Webamp();
+    webamp.renderWhenReady(app);
+
+  },
+  end: async function () {
+    // Close the window when the process is exited
+    wb.close();
+  },
+};
+
+export default pkg;
